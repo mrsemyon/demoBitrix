767bcf141eabb3ff9cd7a5ac77da41fd15df83cd
candaceb <candaceb@student.21-school.ru>
2023-10-17 13:16:09 +0500

    added footer layout

--- local/templates/demoBitrix/footer.php
+++ local/templates/demoBitrix/footer.php
@@ -1,4 +1,205 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
-<p>It's footer...</p>
-</body>
+      <!--footer-->
+      <footer
+        id="t-footer"
+        class="t-records"
+        data-hook="blocks-collection-content-node"
+        data-tilda-project-id="7048371"
+        data-tilda-page-id="37080433"
+        data-tilda-formskey="1fdfb8c7ab860107f35ce02be7048371"
+        data-tilda-lazy="yes"
+        data-tilda-project-headcode="yes"
+      >
+        <div
+          id="rec599066639"
+          class="r t-rec t-rec_pt_0 t-rec_pb_15"
+          style="
+            padding-top: 0px;
+            padding-bottom: 15px;
+            background-color: #ffffff;
+          "
+          data-animationappear="off"
+          data-record-type="460"
+          data-bg-color="#ffffff"
+        >
+          <!-- t460 --><!-- cover --><!--[if IE 8
+            ]><style>
+              .t460 .t-sociallinks__item a:before {
+                background: none !important;
+              }
+              .t460 .t-sociallinks__item a {
+                overflow: visible !important;
+              }
+            </style><!
+          [endif]-->
+          <div class="t460">
+            <div class="t-container">
+              <div class="t460__wrapper">
+                <div
+                  class="t460__bottomtext t-descr t-descr_xxs"
+                  field="text"
+                  style="
+                    text-align: center;
+                    color: #2e2d2c;
+                    color: #2e2d2c;
+                    font-family: 'Circe';
+                  "
+                >
+                  © Рецифра, 2023.<br />Любое копирование информации с данного
+                  сайта, разрешено только с указанием активной ссылки на
+                  источник<br /><a
+                    href="https://recifra.ru/policy"
+                    style="
+                      color: rgb(46, 45, 44);
+                      border-bottom: 2px solid rgb(46, 45, 44);
+                      box-shadow: none;
+                      text-decoration: none;
+                    "
+                    >Политика обработки данных</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          id="rec646316925"
+          class="r t-rec"
+          style=""
+          data-animationappear="off"
+          data-record-type="972"
+        >
+          <!-- T972 -->
+
+          <style>
+            @media (hover: hover), (min-width: 0\0) {
+              #rec646316925 .t-btn:not(.t-animate_no-hover):hover {
+                color: #ffffff !important;
+                background-color: #22c213 !important;
+              }
+              #rec646316925 .t-btn:not(.t-animate_no-hover):focus-visible {
+                color: #ffffff !important;
+                background-color: #22c213 !important;
+              }
+              #rec646316925 .t-btntext:not(.t-animate_no-hover):hover {
+                color: #ffffff !important;
+              }
+            }
+          </style>
+          <script type="text/javascript">
+            t_onReady(function () {
+              setTimeout(function () {
+                t_onFuncLoad("t972_init", function () {
+                  t972_init("646316925");
+                });
+              }, 500);
+            });
+          </script>
+          <style type="text/css">
+            #rec646316925 .t972__toggle-input:checked + .t972__toggle-container .t972__toggle-mark {
+            background-color: #22c213;
+            }
+            #rec646316925 .t972__toggle-input:checked + .t972__toggle-container .t972__toggle-background {
+            background-color: #22c213;
+            }
+            #rec646316925 .t972__category-enabled {
+            color: #22c213;
+            }
+            #rec646316925 .t972__category-enabled_always {
+            color: #22c213;
+            }
+            {if $a.leftleft<>"" || $a.rightright<>""}
+            @media screen and (max-width: 640px) {
+            #rec646316925 .t972 {
+            bottom: 10px; width: calc(100% - * 2);
+            }
+            }
+            {/if}
+          </style>
+        </div>
+      </footer>
+      <!--/footer-->
+    </div>
+    <!--/allrecords--><!-- Stat --><!-- Yandex.Metrika counter 93681979 -->
+    <script type="text/plain" data-tilda-cookie-type="analytics">
+      setTimeout(function(){ (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); window.mainMetrikaId = 93681979; ym(window.mainMetrikaId , "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); }, 2000);
+    </script>
+    <noscript
+      ><div>
+        <img
+          src="https://mc.yandex.ru/watch/93681979"
+          style="position: absolute; left: -9999px"
+          alt=""
+        /></div
+    ></noscript>
+    <!-- /Yandex.Metrika counter -->
+    <script type="text/plain" data-tilda-cookie-type="analytics">
+      setTimeout(function(){ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'G-4QTQHT6MZF', 'auto');
+      ga('send', 'pageview');
+      }, 2000); window.mainTracker = 'user';
+    </script>
+    <script type="text/javascript">
+      if (!window.mainTracker) {
+        window.mainTracker = "tilda";
+      }
+      window.tildastatcookiegdpr = "yes";
+      window.tildastatscroll = "yes";
+      setTimeout(function () {
+        (function (d, w, k, o, g) {
+          var n = d.getElementsByTagName(o)[0],
+            s = d.createElement(o),
+            f = function () {
+              n.parentNode.insertBefore(s, n);
+            };
+          s.type = "text/javascript";
+          s.async = true;
+          s.key = k;
+          s.id = "tildastatscript";
+          s.src = g;
+          if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+          } else {
+            f();
+          }
+        })(
+          document,
+          window,
+          "5540e56455aef3cf6956f59db8a4def4",
+          "script",
+          "assets/js/tilda-stat-1.0.min.js"
+        );
+      }, 2000);
+    </script>
+    <!-- Rating Mail.ru counter -->
+    <script type="text/plain" data-tilda-cookie-type="analytics">
+      setTimeout(function(){
+      var _tmr = window._tmr || (window._tmr = []);
+      _tmr.push({id: "3405143", type: "pageView", start: (new Date()).getTime()});
+      window.mainMailruId = '3405143';
+      (function (d, w, id) {
+      if (d.getElementById(id)) {return;}
+      var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+      ts.src = "https://top-fwz1.mail.ru/js/code.js";
+      var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+      if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+      })(document, window, "topmailru-code");
+      }, 2000);
+    </script>
+    <noscript
+      ><img
+        src="https://top-fwz1.mail.ru/counter?id=3405143;js=na"
+        style="
+          border: 0;
+          position: absolute;
+          left: -9999px;
+          width: 1px;
+          height: 1px;
+        "
+        alt="Top.Mail.Ru" /></noscript
+    ><!-- //Rating Mail.ru counter -->
+  </body>
 </html>
