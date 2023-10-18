1d4773c4360a45e2761b5c423c5d5a2b044d76c4
candaceb <candaceb@student.21-school.ru>
2023-10-18 12:22:39 +0500

    added include aria for skills

--- index.php
+++ index.php
@@ -3134,151 +3134,17 @@ $APPLICATION->SetTitle("Студенческое Диджитал Многобо
         </script>
         <!-- /T396 -->
       </div>
-      <div
-        id="rec612854904"
-        class="r t-rec t-rec_pt_150 t-rec_pb_180"
-        style="padding-top: 150px; padding-bottom: 180px"
-        data-record-type="502"
-      >
-        <!-- t502 -->
-        <div class="t502">
-          <ul
-            role="list"
-            class="t502__container t-list__container_inrow3 t-container"
-          >
-            <li
-              class="t502__col t-col t-col_4 t-list__item t-align_left t-item"
-            >
-              <div class="t502__tablewrapper">
-                <div class="t502__circlewrapper">
-                  <div
-                    class="t502__circle t-title"
-                    style="background-color: #2ce817; color: #2e2d2c"
-                  >
-                    1
-                  </div>
-                </div>
-                <div class="t502__textwrapper" style="vertical-align: middle">
-                  <div
-                    class="t502__title t-name t-name_sm"
-                    style="color: #2e2d2c; font-size: 20px; padding-bottom: 0"
-                    field="li_title__1476980540792"
-                  >
-                    Выполнишь реальные рабочие задачи и&nbsp;оценишь свои силы
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li
-              class="t502__col t-col t-col_4 t-list__item t-align_left t-item"
-            >
-              <div class="t502__tablewrapper">
-                <div class="t502__circlewrapper">
-                  <div
-                    class="t502__circle t-title"
-                    style="background-color: #2ce817; color: #2e2d2c"
-                  >
-                    2
-                  </div>
-                </div>
-                <div class="t502__textwrapper" style="vertical-align: middle">
-                  <div
-                    class="t502__title t-name t-name_sm"
-                    style="color: #2e2d2c; font-size: 20px; padding-bottom: 0"
-                    field="li_title__1476980557179"
-                  >
-                    Поймешь, какие навыки нужны работодателям
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li
-              class="t502__col t-col t-col_4 t-list__item t-align_left t-item"
-            >
-              <div class="t502__tablewrapper">
-                <div class="t502__circlewrapper">
-                  <div
-                    class="t502__circle t-title"
-                    style="background-color: #2ce817; color: #2e2d2c"
-                  >
-                    3
-                  </div>
-                </div>
-                <div class="t502__textwrapper" style="vertical-align: middle">
-                  <div
-                    class="t502__title t-name t-name_sm"
-                    style="color: #2e2d2c; font-size: 20px; padding-bottom: 0"
-                    field="li_title__1476980560541"
-                  >
-                    Сможешь выиграть стажировку в&nbsp;компании
-                    с&nbsp;возможностью дальнейшего трудоустройства
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li
-              class="t502__col t-col t-col_4 t-list__item t-align_left t-item"
-            >
-              <div class="t502__tablewrapper">
-                <div class="t502__circlewrapper">
-                  <div
-                    class="t502__circle t-title"
-                    style="background-color: #2ce817; color: #2e2d2c"
-                  >
-                    4
-                  </div>
-                </div>
-                <div class="t502__textwrapper" style="vertical-align: middle">
-                  <div
-                    class="t502__title t-name t-name_sm"
-                    style="color: #2e2d2c; font-size: 20px; padding-bottom: 0"
-                    field="li_title__1476980563735"
-                  >
-                    В&nbsp;комфортной домашней обстановке и&nbsp;без стресса,
-                    создашь кейсы для&nbsp;портфолио и&nbsp;резюме
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li
-              class="t502__col t-col t-col_4 t-list__item t-align_left t-item"
-            >
-              <div class="t502__tablewrapper">
-                <div class="t502__circlewrapper">
-                  <div
-                    class="t502__circle t-title"
-                    style="background-color: #2ce817; color: #2e2d2c"
-                  >
-                    5
-                  </div>
-                </div>
-                <div class="t502__textwrapper" style="vertical-align: middle">
-                  <div
-                    class="t502__title t-name t-name_sm"
-                    style="color: #2e2d2c; font-size: 20px; padding-bottom: 0"
-                    field="li_title__1476980566485"
-                  >
-                    Сможешь завести новые знакомства в&nbsp;комьюнити
-                    единомышленников
-                  </div>
-                </div>
-              </div>
-            </li>
-          </ul>
-          <style>
-            @media (hover: hover), (min-width: 0\0) {
-              #rec612854904 .t-btn:not(.t-animate_no-hover):hover {
-                color: #ffffff !important;
-                background-color: #22c213 !important;
-              }
-              #rec612854904 .t-btn:not(.t-animate_no-hover):focus-visible {
-                color: #ffffff !important;
-                background-color: #22c213 !important;
-              }
-            }
-          </style>
-        </div>
-      </div>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"AREA_FILE_SHOW" => "file",
+		"PATH" => "./skills_inc.php",
+		"EDIT_TEMPLATE" => ""
+	),
+	false
+);?>
       <div
         id="rec612845875"
         class="r t-rec"
