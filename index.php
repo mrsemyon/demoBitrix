0c41c30bed90bd340aa8b9280a3c0befb48bdc97
candaceb <candaceb@student.21-school.ru>
2023-10-18 13:19:02 +0500

    added include aria for partners

--- index.php
+++ index.php
@@ -5473,92 +5473,17 @@ $APPLICATION->SetTitle("Студенческое Диджитал Многобо
         </script>
         <!-- /T396 -->
       </div>
-      <div
-        id="rec651107635"
-        class="r t-rec t-rec_pt_90 t-rec_pb_150 t-screenmin-640px"
-        style="
-          padding-top: 90px;
-          padding-bottom: 150px;
-          background-color: #2e2d2c;
-        "
-        data-record-type="594"
-        data-screen-min="640px"
-        data-bg-color="#2e2d2c"
-      >
-        <!-- t594 -->
-        <div class="t594">
-          <div class="t594__container t-card__container t-container">
-            <div
-              class="t-col t-card__col t-card__col_withoutbtn t594__item t594__item_4-in-row"
-            >
-              <a
-                href="https://depit.admhmao.ru/"
-                class="t-card__link"
-                target="_blank"
-                ><img
-                  class="t594__img t-img"
-                  src="images/cifraYGRA.png"
-                  data-original="<?=ASSETS?>/images/tild3637-6635-4839-a361-376632616133__photo_2023-10-09_16-.png"
-                  imgfield="li_img__0107122420240"
-                  style="max-width: 400px"
-                  alt=""
-              /></a>
-            </div>
-            <div
-              class="t-col t-card__col t-card__col_withoutbtn t594__item t594__item_4-in-row"
-            >
-              <a href="https://gp-it.ru/" class="t-card__link" target="_blank"
-                ><img
-                  class="t594__img t-img"
-                  src="images/1cGoodPeople.png"
-                  data-original="<?=ASSETS?>/images/tild3464-6137-4039-b335-316135316430__frame_3929.png"
-                  imgfield="li_img__0107122420241"
-                  style="max-width: 400px"
-                  alt=""
-              /></a>
-            </div>
-            <div
-              class="t-col t-card__col t-card__col_withoutbtn t594__item t594__item_4-in-row"
-            >
-              <a
-                href="https://vk.com/pruginahm"
-                class="t-card__link"
-                target="_blank"
-                ><img
-                  class="t594__img t-img"
-                  src="images/Pryshina.png"
-                  data-original="<?=ASSETS?>/images/tild3765-3563-4834-b734-613030316436__photo.png"
-                  imgfield="li_img__1697024645335"
-                  style="max-width: 400px"
-                  alt=""
-              /></a>
-            </div>
-            <div
-              class="t-col t-card__col t-card__col_withoutbtn t594__item t594__item_4-in-row"
-            >
-              <a
-                href="https://or86.myopenugra.ru/"
-                class="t-card__link"
-                target="_blank"
-                ><img
-                  class="t594__img t-img"
-                  src="images/OpenRegion.png"
-                  data-original="<?=ASSETS?>/images/tild6138-3966-4435-b664-306130353437__photo.png"
-                  imgfield="li_img__1697033097888"
-                  style="max-width: 400px"
-                  alt=""
-              /></a>
-            </div>
-          </div>
-        </div>
-        <script>
-          t_onReady(function () {
-            t_onFuncLoad("t594_init", function () {
-              t594_init("651107635");
-            });
-          });
-        </script>
-      </div>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"AREA_FILE_SHOW" => "file",
+		"PATH" => "./partners_inc.php",
+		"EDIT_TEMPLATE" => ""
+	),
+	false
+);?>
       <div
         id="rec651368707"
         class="r t-rec t-rec_pt_60 t-rec_pb_150 t-screenmax-640px"
