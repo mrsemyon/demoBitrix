42d367b1ca15cc219c90cc2b4abe85d9483f393a
candaceb <candaceb@student.21-school.ru>
2023-10-18 13:16:52 +0500

    added include aria for organizers

--- index.php
+++ index.php
@@ -5084,85 +5084,17 @@ $APPLICATION->SetTitle("Студенческое Диджитал Многобо
         </script>
         <!-- /T396 -->
       </div>
-      <div
-        id="rec612845883"
-        class="r t-rec t-rec_pt_90 t-rec_pb_90"
-        style="padding-top: 90px; padding-bottom: 90px"
-        data-record-type="600"
-      >
-        <!-- t600 -->
-        <div class="t600">
-          <ul role="list" class="t600__container t-container">
-            <li class="t-list__item t-col t-col_12 t-prefix_">
-              <div class="t600__img-cell-wrapper t-cell t-valign_middle">
-                <div class="t600__imgwrapper" style="width: 300px">
-                  <img
-                    class="t600__img t-img"
-                    src="<?=ASSETS?>/images/tild3362-3432-4361-b132-326430653735__photo.svg"
-                    imgfield="li_img__1481528188995"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div
-                class="t600__textwrapper t600__textwr_leftpadding t-cell t-valign_middle"
-                style=""
-              >
-                <div
-                  class="t600__title t-name t-name_xl"
-                  style=""
-                  field="li_title__1481528188995"
-                >
-                  <p style="text-align: left">
-                    <a
-                      href="https://recifra.ru/"
-                      style="color: rgb(44, 232, 23)"
-                      target="_blank"
-                      rel="noreferrer noopener"
-                      >Рецифра</a
-                    >&nbsp;— аккредитованная&nbsp;IT компания, топ-10
-                    в&nbsp;рейтинге digital-подрядчиков для&nbsp;госсектора
-                    (Рейтинг Рунета 2023), 14 лет на&nbsp;рынке
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li class="t-list__item t-col t-col_12 t-prefix_">
-              <div class="t600__img-cell-wrapper t-cell t-valign_middle">
-                <div class="t600__imgwrapper" style="width: 300px">
-                  <img
-                    class="t600__img t-img"
-                    src="<?=ASSETS?>/images/tild6237-3031-4138-a661-303235653239__foot-logo_1.svg"
-                    imgfield="li_img__1481528175762"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div
-                class="t600__textwrapper t600__textwr_leftpadding t-cell t-valign_middle"
-                style=""
-              >
-                <div
-                  class="t600__title t-name t-name_xl"
-                  style=""
-                  field="li_title__1481528175762"
-                >
-                  <p style="text-align: left">
-                    <a
-                      href="https://www.ugrasu.ru/"
-                      style="color: rgb(44, 232, 23)"
-                      target="_blank"
-                      rel="noreferrer noopener"
-                      >Югорский Государственный Университет</a
-                    >&nbsp;— одно из&nbsp;главных учебных заведений высшего
-                    образования Югры, основанное в&nbsp;2001 году
-                  </p>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"AREA_FILE_SHOW" => "file",
+		"PATH" => "./organizers_inc.php",
+		"EDIT_TEMPLATE" => ""
+	),
+	false
+);?>
       <div
         id="rec640128196"
         class="r t-rec t-rec_pt_0 t-rec_pb_120"
